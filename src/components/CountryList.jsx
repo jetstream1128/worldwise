95cf66d6b8f34b530ae73f165e39c2b6a9146241
jetstream1128 <bogdanalexeev2007@live.com>
2024-02-24 22:46:06 -0700

    first

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
+
+function CountryList() {
+	const { cities, isLoading } = useCities();
+	if (isLoading) return <Spinner />;
+
+	if (!cities.length)
+		return <Message message="Add your first city by clicking on a city map" />;
+
+	const countries = cities.reduce((arr, city) => {
+		if (!arr.map((el) => el.country).includes(city.country))
+			return [
+				...arr,
+				{ country: city.country, emoji: city.emoji, id: city.id },
+			];
+		else return arr;
+	}, []);
+
+	return (
+		<ul className={styles.countryList}>
+			{countries.map((country) => (
+				<CountryItem country={country} key={country.id} />
+			))}
+		</ul>
+	);
+}
+export default CountryList;
